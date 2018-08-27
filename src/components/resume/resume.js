e5c0a2e4a7266844b2726f111d50d81bd8d3d24e
randolrs <r.shane.randolph@gmail.com>
2018-08-26 20:37:42 -0400

    add page header, resume google doc cta

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -11,17 +11,34 @@ class Resume extends Component {
 					<p>{resumeEntry.description}</p>
 				</div>
 			);
-		}); 
+		});
+		let resumeGoogleDocsLink = 'https://drive.google.com/file/d/1BdNGmUMf6u81FuullmebrL0x_E3xFlmz/view?usp=sharing'; 
     	return (
-			<div className="content">
-				<h1><span>Resume</span></h1>
-				<h2><span>TLDR: I am a front-end developer that has shipped clean, high performance, well-tested code for 6 years</span></h2>
-				<div>
-					{resume}
-				</div>
+    		<div>
+    			<div className="page-header centered">
+    				<div className="page-header__content centered">	
+	    				<img src="/Shane.png" className="home__content__img"/>
+	    				<h1><span>My Resume</span></h1>
+	    				<h2><span>TLDR: I am a front-end developer that has shipped clean, high performance, well-tested code for 7 years</span></h2>
+	    				<div>
+		    				<a
+								href={resumeGoogleDocsLink}
+								target="_blank"
+								className="content__main-cta"
+							>
+								<span>View/Download Google Docs Version (PDF)</span>
+							</a>
+						</div>
+					</div>
+    			</div>
+				<div className="content">
+					<div>
+						{resume}
+					</div>
 
-				<div>
-					<span>Projects CTA</span>
+					<div>
+						<span>Projects CTA</span>
+					</div>
 				</div>
 			</div>
 		)
